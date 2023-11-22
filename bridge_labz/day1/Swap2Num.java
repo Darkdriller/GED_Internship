1ede8942497c26ded823bd9e4e1a47e82f5fd436
Exypnos <swaindhruv28@gmail.com>
2023-11-22 10:43:58 +0530

    Added: Swap2Num.java

---
+++ bridge_labz/day1/Swap2Num.java
@@ -0,0 +1,15 @@
+public class Swap2Num {
+    public static void swap(int a, int b) {
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        swap(a, b);
+    }
+}
